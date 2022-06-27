868a66f07bca90a910539f069054f0e7eafd7237
lxh15 <1392649188@qq.com>
2022-06-27 21:24:44 +0800

    处理less文件

--- scr/index.js
+++ scr/index.js
@@ -14,3 +14,6 @@ $('#swiper').css('background-color', 'red')
 
 // 引入css文件 
 import './styles/index.css'
+
+// 引入less文件 
+import './styles/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,15 @@ module.exports = {
                 // loader 执行 从右往左
                 use: ['style-loader', 'css-loader']
             },
+            {
+                test: /\.less$/, // 匹配执行类型的文件
+                // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+                // 执行的顺序 less-loader css-loader style-loader
+                // less-loader 先把less代码转换成css
+                // css-loader 再把css代码转换成webpack 可以识别的js代码
+                // style-loader 在把css代码插入到 dom中
+                use: ["style-loader", "css-loader", 'less-loader']
+            },
         ]
     }
 
